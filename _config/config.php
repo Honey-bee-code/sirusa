ab4318423dc1b18eafacce8d04c3536553956c89
Honey-bee-code <hanifinida@gmail.com>
2021-02-06 16:34:03 +0800

    login and logout

---
+++ _config/config.php
@@ -0,0 +1,19 @@
+<?php
+date_default_timezone_set('Asia/Makassar');
+session_start();
+
+$koneksi = mysqli_connect('localhost', 'root', '', 'sirusa');
+if(mysqli_connect_errno()) {
+    echo mysqli_connect_error();
+} 
+
+function base_url($url = null) {
+    $base_url = "http://localhost/sirusa";
+    if($url != null) {
+        return $base_url."/".$url;
+    } else {
+        return $base_url;
+    }
+}
+
+?>
--- auth/login.php
+++ auth/login.php
@@ -1,3 +1,7 @@
+<?php
+require_once "../_config/config.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,13 +12,36 @@
     <meta name="author" content="">
     <title>Login - SiRuSa</title>
     <!-- Bootstrap Core CSS -->
-    <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?=base_url('_assets/css/bootstrap.min.css')?>" rel="stylesheet">
     <!-- Custom CSS -->
-    <link href="../assets/css/simple-sidebar.css" rel="stylesheet">
+    <link href="<?=base_url('_assets/css/simple-sidebar.css')?>" rel="stylesheet">
 </head>
 <body>
     <div class="container">
         <div align="center" style="margin-top: 200px">
+            <?php
+            if(isset($_POST['login'])) {
+                $user = trim(mysqli_real_escape_string($koneksi, $_POST['username']));
+                $password = sha1(trim(mysqli_real_escape_string($koneksi, $_POST['password'])));
+                // tambahkan or die sebagai trancking error
+                $sql_login = mysqli_query($koneksi, "SELECT * FROM user WHERE username = '$user' AND password = '$password'") or die (mysqli_error($koneksi));
+                if(mysqli_num_rows($sql_login) > 0) {
+                    $_SESSION['user'] = $user;
+                    echo "<script>window.location='".base_url()."';</script>";
+                } else { ?>
+                    <div class="row">
+                        <div class="col-lg-6 col-lg-offset-3">
+                            <div class="alert alert-danger alert-dismissable" role="alert">
+                                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                                <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+                                <strong>Login gagal!</strong> Username / Password salah
+                            </div>
+                        </div>
+                    </div>
+                <?php
+                }
+            }
+            ?>
             <form action="" method="post" class="navbar-form">
                 <div class="input-group">
                     <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
@@ -34,5 +61,7 @@
             </form>
         </div>
     </div>
+    <script src="<?=base_url('_assets/js/jquery.js')?>"></script>
+    <script src="<?=base_url('_assets/js/bootstrap.min.js')?>"></script>
 </body>
 </html>
