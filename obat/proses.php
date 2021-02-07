3caa2e475e596d11c38477c557f23bea1a813363
Honey-bee-code <hanifinida@gmail.com>
2021-02-07 18:08:03 +0800

    edit data obat

--- obat/proses.php
+++ obat/proses.php
@@ -9,8 +9,12 @@ if(isset($_POST['tambah'])) {
     $ket = trim(mysqli_real_escape_string($koneksi, $_POST['ket']));
     mysqli_query($koneksi, "INSERT INTO obat (id_obat, nama_obat, ket_obat) VALUES ('$uuid', '$nama', '$ket')") or die (mysqli_error($koneksi));
     echo "<script>window.location='data.php';</script>";
-} else if(isset($_POST['tambah'])) {
-
+} else if(isset($_POST['edit'])) {
+    $id = $_POST['id'];
+    $nama = trim(mysqli_real_escape_string($koneksi, $_POST['nama']));
+    $ket = trim(mysqli_real_escape_string($koneksi, $_POST['ket']));
+    mysqli_query($koneksi, "UPDATE obat SET nama_obat = '$nama', ket_obat = '$ket' WHERE id_obat = '$id'") or die (mysqli_error($koneksi));
+    echo "<script>window.location='data.php';</script>";
 } else {
     echo "<script>window.location='data.php';</script>";
 }
