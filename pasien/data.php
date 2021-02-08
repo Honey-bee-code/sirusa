fe425e074acdbd8523154c0f1a0675bcdfb00f07
Honey-bee-code <hanifinida@gmail.com>
2021-02-08 20:37:58 +0800

    add pasien folder

---
+++ pasien/data.php
@@ -0,0 +1,29 @@
+<?php include_once('../_header.php');?>
+<div class="box">
+    <h1>Pasien</h1>
+    <h4>
+        <small>Data Pasien</small>
+        <div class="pull-right">
+            <a href="" class="btn btn-default btn-xs"><i class="glyphicon glyphicon-refresh"></i> Refresh</a>
+            <a href="tambah.php" class="btn btn-success btn-xs"><i class="glyphicon glyphicon-plus"></i> Tambah Pasien</a>
+        </div>
+    </h4>
+        <div class="table-responsive">
+            <table class="table table-striped table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th width="20px">No.</th>
+                        <th>Nama Dokter</th>
+                        <th>Spesialis</th>
+                        <th>Alamat</th>
+                        <th>No. Telepon</th>
+                        <th class="text-center"><i class="glyphicon glyphicon-cog"></i></th>
+                    </tr>
+                </thead>
+                
+            </table>
+        </div>
+    
+</div>
+
+<?php include_once('../_footer.php');?>
